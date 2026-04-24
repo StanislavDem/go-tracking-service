dce5fdfd11633e288e9e81854a3047bc0dff71b2
StanislavDem <fars1r@outlook.com>
2026-04-24 20:36:24 +0500

    Implement CRUD functions and tests for Parcel

--- main.go
+++ main.go
@@ -67,25 +67,25 @@ func (s ParcelService) PrintClientParcels(client int) error {
 	return nil
 }
 
-func (s ParcelService) NextStatus(number int) error {
+func (s ParcelService) NewStatus(number int) error {
 	parcel, err := s.store.Get(number)
 	if err != nil {
 		return err
 	}
 
-	var nextStatus string
+	var newStatus string
 	switch parcel.Status {
 	case ParcelStatusRegistered:
-		nextStatus = ParcelStatusSent
+		newStatus = ParcelStatusSent
 	case ParcelStatusSent:
-		nextStatus = ParcelStatusDelivered
+		newStatus = ParcelStatusDelivered
 	case ParcelStatusDelivered:
 		return nil
 	}
 
-	fmt.Printf("У посылки № %d новый статус: %s\n", number, nextStatus)
+	fmt.Printf("У посылки № %d новый статус: %s\n", number, newStatus)
 
-	return s.store.SetStatus(number, nextStatus)
+	return s.store.SetStatus(number, newStatus)
 }
 
 func (s ParcelService) ChangeAddress(number int, address string) error {
@@ -97,9 +97,15 @@ func (s ParcelService) Delete(number int) error {
 }
 
 func main() {
-	// настройте подключение к БД
+	// подключение к БД tracker.db
+	db, err := sql.Open("sqlite", "tracker.db")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer db.Close()
 
-	store := // создайте объект ParcelStore функцией NewParcelStore
+	store := NewParcelStore(db) // создаём объект ParcelStore и передаём в NewParcelStore
 	service := NewParcelService(store)
 
 	// регистрация посылки
@@ -120,7 +126,7 @@ func main() {
 	}
 
 	// изменение статуса
-	err = service.NextStatus(p.Number)
+	err = service.NewStatus(p.Number)
 	if err != nil {
 		fmt.Println(err)
 		return
